5bd3f70cd1fbf79f1f72feecafd21ecb29967909
José Winny <jose.winny@icloud.com>
2023-05-25 18:09:21 -0300

    criacao do xcodeproject

---
+++ LoveMyCapibara/LoveMyCapibara/LoveMyCapibaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveMyCapibaraApp.swift
+//  LoveMyCapibara
+//
+//  Created by userext on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LoveMyCapibaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a7b8fc83b4db4f02502a21c75780fa9063a80fba
Tiago Vargas <tiago.ce10@outlook.com>
2023-05-25 21:45:44 -0300

    Add tab view and its view files as stubs

--- LoveMyCapibara/LoveMyCapibara/Views/Scenes/ContentView.swift
+++ LoveMyCapibara/LoveMyCapibara/Views/Scenes/ContentView.swift
@@ -4,19 +4,22 @@
 //
 //  Created by userext on 25/05/23.
 //
-//TO DO: Update name
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "pawprint")
+                }
+
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape")
+                }
         }
-        .padding()
     }
 }
 

af7de69b5b84ba48b961ed0fc9a874473a0fa01a
gusholz <gustavobarretohv@live.com>
2023-05-30 18:01:01 -0300

    UI.Screen.width changed to GeometryReader to prevent future bugs

--- LoveMyCapibara/LoveMyCapibara/Views/ReusableComponents/PetCardView.swift
+++ LoveMyCapibara/LoveMyCapibara/Views/ReusableComponents/PetCardView.swift
@@ -13,29 +13,32 @@ struct PetCardView: View {
     var petImage: String
     
     var body: some View {
-        HStack(){
-            Image(petImage)
-                .clipShape(Circle())
-                .padding(.trailing,4)
-            VStack(alignment: .leading){
-                Text(petName)
-                    .font(FontManager.poppinsBold(size: 16))
-                    .foregroundColor(Color("PrimaryText"))
-                Text(petSpecies)
-                    .font(FontManager.poppinsRegular(size: 13))
-                    .foregroundColor(Color("PrimaryText"))
+        GeometryReader{ geometry in
+            HStack(){
+                Image(petImage)
+                    .clipShape(Circle())
+                    .padding(.trailing,4)
+                VStack(alignment: .leading){
+                    Text(petName)
+                        .font(FontManager.poppinsBold(size: 16))
+                        .foregroundColor(Color("PrimaryText"))
+                    Text(petSpecies)
+                        .font(FontManager.poppinsRegular(size: 13))
+                        .foregroundColor(Color("PrimaryText"))
+                }
             }
+            .frame(width: geometry.size.width * 0.8,alignment: .leading)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color("CardBorderColor"), lineWidth: 2)
+            )
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("CardBackgroundColor"))
+            )
         }
-        .frame(width: UIScreen.main.bounds.width * 0.8,alignment: .leading)
-        .padding()
-        .overlay(
-            RoundedRectangle(cornerRadius: 8)
-                .stroke(Color("CardBorderColor"), lineWidth: 2)
-        )
-        .background(
-            RoundedRectangle(cornerRadius: 8)
-                .fill(Color("CardBackgroundColor"))
-        )
+        
     }
 }
 

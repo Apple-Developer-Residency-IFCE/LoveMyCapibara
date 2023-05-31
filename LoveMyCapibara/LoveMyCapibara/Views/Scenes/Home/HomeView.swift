9b65136292e01980119d190932b7711301b25ba3
Maurrici <mauriciomoura837@gmail.com>
2023-05-31 19:28:02 -0300

    Remove commet of HomeView

--- LoveMyCapibara/LoveMyCapibara/Views/Scenes/Home/HomeView.swift
+++ LoveMyCapibara/LoveMyCapibara/Views/Scenes/Home/HomeView.swift
@@ -33,28 +33,8 @@ struct HomeView: View {
                 }
             }
         }
-        .navigationTitle("Pets")
+        
     }
-
-//    let data = (1...100).map { "Item \($0)" }
-//
-//        let columns = [
-//            GridItem(.flexible()),
-//            GridItem(.flexible()),
-//            GridItem(.flexible())
-//        ]
-//
-//        var body: some View {
-//            ScrollView(.horizontal) {
-//                LazyHGrid(rows: columns, spacing: 20) {
-//                    ForEach(data, id: \.self) { item in
-//                        Text(item)
-//                    }
-//                }
-//                .padding(.horizontal)
-//            }
-//            .frame(maxHeight: 300)
-//        }
     
     struct HomeView_Previews: PreviewProvider {
         static var previews: some View {

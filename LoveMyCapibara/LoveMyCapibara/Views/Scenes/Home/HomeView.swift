4c06822518c15616348f06ac42526685c0284acf
Maurrici <mauriciomoura837@gmail.com>
2023-05-31 19:10:57 -0300

    Add view of list with Pet cards

--- LoveMyCapibara/LoveMyCapibara/Views/Scenes/Home/HomeView.swift
+++ LoveMyCapibara/LoveMyCapibara/Views/Scenes/Home/HomeView.swift
@@ -9,28 +9,52 @@ import SwiftUI
 
 struct HomeView: View {
     @State var homeViewModel = HomeViewModel()
+    
+    let columns = [GridItem(.flexible())]
     var body: some View {
         NavigationView(){
-            List{
-                Group {
-                    NavigationLink{
-                        Text("Info about the Pet")
-                    }label: {
-                        PetCardView(petName: "Bob", petSpecies: "dog", petImage: "PetTestImage")
+            ScrollView{
+                LazyVGrid(columns: columns, spacing: 10){
+                    Group{
+                        NavigationLink{
+                            Text("Tortinha")
+                        }label: {
+                            PetCardView(petName: "Tortinha", petSpecies: "Gato", petImage: "tortinhaImage")
+                        }
+                        
+                        NavigationLink{
+                            Text("Bob")
+                        }label: {
+                            PetCardView(petName: "Bob", petSpecies: "Cachorro", petImage: "PetTestImage")
+                        }
                     }
-                    NavigationLink{
-                        Text("Info about the Pet")
-                    }label: {
-                        PetCardView(petName: "Bob", petSpecies: "dog", petImage: "PetTestImage")
-                    }
+                    .frame(height: 100)
+                    .padding(EdgeInsets(top: 8, leading: 10, bottom: 8, trailing: 10))
                 }
-                .listRowInsets(EdgeInsets(top: 8, leading: 24, bottom: 8, trailing: 34))
-                .frame(height:96)
-                .listRowSeparator(.hidden)
             }
-            .listStyle(.plain)
         }
+        .navigationTitle("Pets")
     }
+
+//    let data = (1...100).map { "Item \($0)" }
+//
+//        let columns = [
+//            GridItem(.flexible()),
+//            GridItem(.flexible()),
+//            GridItem(.flexible())
+//        ]
+//
+//        var body: some View {
+//            ScrollView(.horizontal) {
+//                LazyHGrid(rows: columns, spacing: 20) {
+//                    ForEach(data, id: \.self) { item in
+//                        Text(item)
+//                    }
+//                }
+//                .padding(.horizontal)
+//            }
+//            .frame(maxHeight: 300)
+//        }
     
     struct HomeView_Previews: PreviewProvider {
         static var previews: some View {

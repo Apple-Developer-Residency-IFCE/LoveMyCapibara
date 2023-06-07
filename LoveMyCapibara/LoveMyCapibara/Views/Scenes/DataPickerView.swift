480b386bc76432551def1c91f27c2e36ac43af53
José Winny <jose.winny@icloud.com>
2023-06-07 20:54:58 -0300

    Update DataPickerView.swift

--- LoveMyCapibara/LoveMyCapibara/Views/Scenes/DataPickerView.swift
+++ LoveMyCapibara/LoveMyCapibara/Views/Scenes/DataPickerView.swift
@@ -10,12 +10,8 @@ import SwiftUI
 struct DataPickerView: View {
     @State var birthdayDate : Date = Date.now
     var body: some View {
-        DatePicker("Nascimento",selection: $birthdayDate, displayedComponents: .date)
-            .padding()
-            .background {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(Color("SelectionFieldBackground"))
-            }
+        DatePicker("Nascimento",selection: $birthdayDate, in: ...Date.now, displayedComponents: .date)
+            .padding([.horizontal],16)
             .font(Font.custom("Poppins-Regular", size: 16))
             .foregroundColor(Color("PrimaryText"))
     }
@@ -24,6 +20,6 @@ struct DataPickerView: View {
 struct DataPickerView_Previews: PreviewProvider {
     static var previews: some View {
         DataPickerView()
-            .preferredColorScheme(.dark)
+//            .preferredColorScheme(.dark)
     }
 }
